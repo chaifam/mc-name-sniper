5cf7b44611521f6abf8fbed6ff3df916316bf300
chaifam <justin.by.chai@gmail.com>
2020-07-28 19:59:14 -0400

    Update snipergetdown7-29-2020.py

--- snipergetdown7-29-2020.py
+++ snipergetdown7-29-2020.py
@@ -4,6 +4,7 @@ import json
 
 username = input("Whats your username?\n")
 password = input("How about your password:\n")
+usernameid = input("whats your UUID?\n")
 data = json.dumps({"agent":{"name":"Minecraft","version":1},"username":username,"password":password,"clientToken":""})
 headersforat = {'Content-Type': 'application/json'}
 data = requests.post('https://authserver.mojang.com/authenticate', data=data, headers=headersforat)
@@ -14,13 +15,13 @@ AT = pullData["accessToken"]
 URL = "https://api.mojang.com/user/profile/"
 URL2 = "/skin"  
 # location given here 
-username = "75da7aef40b942dda249ecd628a64319"
+
 headers = {"Authorization": "Bearer "+AT}
-data = {"model":"", "url":"http://assets.mojang.com/SkinTemplates/alex.png"}
+data = {"model":"", "url":"minecraftskins.com/uploads/skins/2020/07/28/sad-boy----2-0-14922982.png?v243"}
 # defining a params dict for the parameters to be sent to the API 
 
   
 # sending get request and saving the response as response object 
-r = requests.post(url =  URL+username+URL2, headers = headers, data=data) 
+r = requests.post(url =  URL+usernameid+URL2, headers = headers, data=data) 
 
 print("the sniper ducked")
