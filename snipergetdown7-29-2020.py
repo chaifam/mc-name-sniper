458de32892b56a4635392d2dd41886d2cfe6fabb
MikiMcC <miki.mccowan@gmail.com>
2020-07-29 03:28:14 +0200

    Update snipergetdown7-29-2020.py

--- snipergetdown7-29-2020.py
+++ snipergetdown7-29-2020.py
@@ -5,18 +5,22 @@ import ntplib
 from time import ctime
 from datetime import datetime
 from pytz import timezone
-
-eastern = timezone('US/Eastern')
-c = ntplib.NTPClient()
-# Provide the respective ntp server ip in below function
-response = c.request('us.pool.ntp.org', version=1)
-response.offset
-print (datetime.fromtimestamp(response.tx_time, eastern))
-
+def time():
+	eastern = timezone('US/Eastern')
+	c = ntplib.NTPClient()
+	# Provide the respective ntp server ip in below function
+	response = c.request('us.pool.ntp.org', version=1)
+	response.offset
+	now = datetime.fromtimestamp(response.tx_time, eastern)
+	print(now)
 
 username = input("Whats your username?\n")
 password = input("How about your password:\n")
 usernameid = input("whats your UUID?\n")
+newname = input("what name do you wanna snipe? \n")
+time = input("what time does the name become available?\n")
+rightNowTime();
+
 data = json.dumps({"agent":{"name":"Minecraft","version":1},"username":username,"password":password,"clientToken":""})
 headersforat = {'Content-Type': 'application/json'}
 data = requests.post('https://authserver.mojang.com/authenticate', data=data, headers=headersforat)
