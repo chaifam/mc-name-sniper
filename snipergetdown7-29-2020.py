c57b929ca82d9ab5d50e360ee606ec5702afaa1d
MikiMcC <miki.mccowan@gmail.com>
2020-07-29 03:40:37 +0200

    Update snipergetdown7-29-2020.py

--- snipergetdown7-29-2020.py
+++ snipergetdown7-29-2020.py
@@ -12,6 +12,7 @@ def rightNowTime():
 	response = c.request('us.pool.ntp.org', version=1)
 	response.offset
 	now = datetime.fromtimestamp(response.tx_time, eastern)
+	return(now)
 
 	
 
@@ -27,8 +28,8 @@ username = input("Whats your username?\n")
 password = input("How about your password:\n")
 usernameid = input("What's your UUID?\n")
 newname = input("What name do you wanna snipe? \n")
-time = input("What time does the name become available?\n")
-real_time = rightNowTime
+snipeTime = input("What time does the name become available?\n")
+
 
 
 data = json.dumps({"agent":{"name":"Minecraft","version":1},"username":username,"password":password,"clientToken":""})
