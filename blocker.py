21a31acb1045917d00b1218e2987eb93f6e40eb4
chaifam <justin.by.chai@gmail.com>
2020-09-11 15:11:14 -0400

    Update blocker.py

--- blocker.py
+++ blocker.py
@@ -21,30 +21,40 @@ with open("config.json", "r") as f:
 
 class account:
 	newname = input("Input name to be blocked: \n")
-	user = None
 
 	def __init__ (self, email, password):
 		self.email = email
 		self.password = password
+		self.user = MojangUser(self.email, self.password, maximum_pool_size = 3)
 
-	def login(self):
-		self.user = MojangUser(self.email, self.password)
+	def block1(self):
+		if self.user.block_username(newname):
+			print(colored(f"Blocked the username {newname}", "cyan"))
+
+			time.sleep(35)
+			if user.block_username(newname):
+				string = colored("Block confirmed", "green")
+				return string
+			else:
+				string = colored("False positive :(", "magenta")
+				return string
+
+	def block(self):	
+		with concurrent.futures.ThreadPoolExecutor() as executor:
+			results = [executor.submit(self.block1) for req in range(3)]
+			for f in concurrent.futures.as_completed(results):
+				print(f.result)
 
 
-def main():
-	emails = config["email"]
-	passwords = config["password"]
-	accounts = list()
-	for i in emails:
-		new_account = account(i, passwords)
-		accounts.append(new_account)
+emails = config["email"]
+passwords = config["password"]
+accounts = list()
+for i in emails:
+	new_account = account(i, passwords)
+	accounts.append(new_account)
 
-	with concurrent.futures.ThreadPoolExecutor() as executor:
-		for account in accounts:
-			executor.submit(account.login)
-			print(f"Logging in on {accont.user}.")
-
-if __name__ == '__main__':
-	main()
-
+for account in accounts:
+	print(f"Logging in on {account.email} {account.password}.")
+	account.block()
+	
 
