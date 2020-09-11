4c1319e7d38be50ce3e87c01e452316d01dcb64b
chaifam <justin.by.chai@gmail.com>
2020-09-11 00:11:52 -0400

    Update blocker.py

--- blocker.py
+++ blocker.py
@@ -1,9 +1,15 @@
 import time
 import os
 import json
+import random, string
+import concurrent.futures
 from colorama import init
 from termcolor import colored
 from mojang import MojangAPI, MojangUser
+
+#x = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+#y = x + "@gmail.com"
+#print(y)
 
 init()
 
@@ -13,22 +19,40 @@ os.chdir(dir_path)
 with open("config.json", "r") as f:
 	config = json.load(f)
 
-class profile:
+class account:
+
 	def __init__ (self, email, password):
 		self.email = email
 		self.password = password
 
-numAccounts = len(config["emails"])
+	def login(self):
+		user = MojangUser(self.email, self.password)
+		return user
 
-for e in range(numAccounts):
-	c = profile(config["emails"][e], config["passwords"][e])
-	print(c.email, c.password)
 
-user = MojangUser(c.email, c.password)
+def block():	
+	drop_timestamp = MojangAPI.get_drop_timestamp(newname)
+	if not drop_timestamp:
+		print(colored(f"{newname} is not dropping", "cyan"))
+	else:
+		seconds = drop_timestamp - time.time()
+		print(colored(f"{newname} drops in {seconds} seconds", "cyan"))
+		
+	if user.block_username(newname, seconds):
+		print(f"Blocked the username {newname}")
+		time.sleep(35)
+		if user.block_username({newname}):
+			print("Success!")
+		else:
+			print("False positive :(")
+
 newname = input("Input name to be blocked: \n")
-drop_timestamp = MojangAPI.get_drop_timestamp(newname)
-if not drop_timestamp:
-	print(colored(f"{newname} is not dropping", "cyan"))
-else:
-	seconds = drop_timestamp - time.time()
-	print(colored(f"{newname} drops in {seconds} seconds", "cyan"))
+emails = config["email"]
+passwords = config["password"]
+
+accounts = list()
+for i in emails:
+	accounts.append(account(i, passwords))
+
+for obj in accounts:
+	print(obj.login())
